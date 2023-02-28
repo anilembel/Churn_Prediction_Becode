146771bae7fd1f2c6ce33357de7735ba6ceda8b3
Meulemans Philippe <90241977+Laverdure77@users.noreply.github.com>
2023-02-28 08:46:37 +0100

    update clustering folder

--- modeling/clustering.py
+++ modeling/clustering.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import pickle
 
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -11,13 +13,11 @@ from sklearn.preprocessing import OrdinalEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.cluster import KMeans
-from sklearn import set_config
 
-
-
+#----------------------------------------------------------------------------------
 
 # Import datas
-data = pd.read_csv('../2_data cleaning/BankChurners.csv')
+data = pd.read_csv('../data_cleaning/BankChurners.csv')
 
 # Save Attrition columns for Probability estimation
 attrition_flag = data['Attrition_Flag']
@@ -25,11 +25,18 @@ attrition_flag = data['Attrition_Flag']
 # Drop unwanted columns
 data = data.drop(['Attrition_Flag','CLIENTNUM','Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1','Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2'], axis=1)
 
-numeric_features =  data.select_dtypes(include=['number'])
+# Sort numerical and categorical values
+numeric_features = data.select_dtypes(include=['number'])
 categorical_features = data.select_dtypes(exclude=['number'])
 # print(categorical.columns)
 
+# Clean datas
+data = data.replace({
+    'Unknown': np.nan
+})
+
 # Pipeline 
+
 # Preprocessing
 numeric_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='mean')),
@@ -37,22 +44,28 @@ numeric_transformer = Pipeline(steps=[
 ])
 
 categorical_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='constant')),
+    ('imputer', SimpleImputer(strategy='most_frequent')),
     ('encoder', OneHotEncoder())
 ])
 
 preprocessor = ColumnTransformer(
    transformers=[
-    ('numeric', numeric_transformer, numeric_features),
-    ('categorical', categorical_transformer, categorical_features)
+    ('numeric', numeric_transformer, numeric_features.columns),
+    ('categorical', categorical_transformer, categorical_features.columns)
 ]) 
 
 # Model  
-# model = Pipeline(steps=['cluster', KMeans(n_clusters=8, init='random', n_init="auto")] )
-
-
-pipe_steps = [('pre',preprocessor), ('model', KMeans(n_clusters=8, init='random', n_init="auto"))]
+pipe_steps = [
+    ('preprocessing',preprocessor),
+    ('model', KMeans(n_clusters=3, init='random', n_init="auto"))]
 pipe = Pipeline(pipe_steps)
 
-
+# Fit the model
 cluster_model = pipe.fit(data)
+
+# Save the model as pickle file
+filename = './model/test_models/cluster.pkl' 
+with open(filename, 'wb') as file:
+    pickle.dump(cluster_model, file)
+    print("Clustering model saved")
+
